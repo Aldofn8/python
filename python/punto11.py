0f33336337553db6b4026b0ff53481f735c8a554
Aldofernandezca <92488491+Aldofn8@users.noreply.github.com>
2022-04-06 23:03:52 -0300

    asd

---
+++ python/punto11.py
@@ -0,0 +1,13 @@
+
+
+
+n = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+numero = input("ingrese un número positivo: ")
+numero = int(numero)
+
+def suma() :
+    suma = numero + n + 1 / 2
+    print(suma)
+suma()
+
